6ee47710a6192cf51d0628bf3bad601af04f1e64
haolxhe172802 <haolxhe172802@fpt.edu.vn>
2023-09-19 10:53:28 +0700

    ngay19/9/2023

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionComponent from './component/FunctionComponent';
+function App() {
+  return (
+<FunctionComponent/>
+    );
+}
+
+export default App;
